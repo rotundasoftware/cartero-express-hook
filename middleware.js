521b8bc46726aa6d09e41bdbb7ad5a16e751a1cc
pepo viola <pepoviola@gmail.com>
2013-12-30 11:16:35 -0300

    remove comments

--- middleware.js
+++ middleware.js
@@ -82,20 +82,12 @@ module.exports = function( projectDir ) {
 			if( ! parcelMetadata ) return next( new Error( "Could not find parcel \"" + parcelName + "\" in parcel map." ) );
 
 			res.locals.cartero_js = _.map( parcelMetadata.js, function( fileName ) {
-				// don't change file path if its a CDN file
-				//if ( ! /https?:\/\//.test( fileName ) )
-					// trim off leading and trailing slashes from public url, which we'll need to prepend to asset urls later
-				//	fileName = carteroJson.publicUrl.replace( /\/$/, "" ).replace( /^\//, "" ) + fileName.replace( carteroJson.publicDir, "" );
 
 				return "<script type='text/javascript' src='" + fileName + "'></script>";
 				
 			} ).join( "" );
 
 			res.locals.cartero_css = _.map( parcelMetadata.css, function( fileName ) {
-				// don't change file path if its a CDN file
-				//if ( ! /https?:\/\//.test( fileName ) )
-					// trim off leading and trailing slashes from public url, which we'll need to prepend to asset urls later
-				//	fileName = carteroJson.publicUrl.replace( /\/$/, "" ).replace( /^\//, "" ) + fileName.replace( carteroJson.publicDir, "" );
 
 				return "<link rel='stylesheet' href='" + fileName + "'></link>";
 
